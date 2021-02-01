04a57ba7ae4e12551437b437c15ba631a5c3f2ab
Tibor Ukropina <77939216+tibor-ukropina@users.noreply.github.com>
2021-02-01 17:28:24 -0500

    Update arraytester.c

--- arraytester.c
+++ arraytester.c
@@ -1,3 +1,6 @@
+/* 
+This file allows testing of the array API
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "ds_memory.h"
@@ -8,17 +11,19 @@ int main(void){
 
     int i;
 
+    /* we chose to use array.bin to store our array*/
     i = ds_create("array.bin", 2048);
     i = ds_create_array();
     i = ds_init_array();
 
+    /* we will read numbers from the file elements.txt*/
     ds_read_elements("elements.txt");
 
-    
-    
-    printf("i: %d\n", i);
+    /* sample swap, here we are swapping elements at positions 2 and 5 */
+    i = ds_swap(2, 5);
 
-    
+    /* sample delete, here we delete element from 6th position*/
+    i = ds_delete(6);
 
     i = ds_finish_array();
 
